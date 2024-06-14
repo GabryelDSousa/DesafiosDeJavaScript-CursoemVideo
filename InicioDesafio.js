4330d55e6df9c178a126e5aef6021ec2b0d83423
Gabryel <gabryeldeusinosousafreitas@gmail.com>
2024-06-14 19:20:53 -0300

    terminal 2° exercício

---
+++ InicioDesafio.js
@@ -0,0 +1,7 @@
+function boasVindas() {
+    let nome = prompt('Qual é o seu Nome?')
+    let idade = prompt('Qual a sua Idade')
+    if(nome != null) {
+        window.alert(`Acabei de reconhecer você ${nome} de ${idade} Anos de Idade`)
+    }
+} 
